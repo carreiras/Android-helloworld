fa0f08cde221257942e29dad373cb2f733d4403a
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-02-17 16:11:29 -0300

    Fase 1: Executando o layout no código e ajustando a versão do SDK (isso só ocorreu porque o SDK estava desatualizado)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "carreiras.com.github.helloworld"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "carreiras.com.github.helloworld"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/carreiras/com/github/helloworld/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/helloworld/MainActivity.kt
@@ -16,5 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Associa a Activity ao layout definido em activity_main.xml
+        setContentView(R.layout.activity_main)
     }
 }
